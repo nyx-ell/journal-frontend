618cfd4ac09654ffabde54baa53ee09f2efea68a
nyx-ell <nicole.linyx@gmail.com>
2019-04-30 18:20:42 +0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import UserPage from './pages/UserPage';
+import NewJournal from './pages/NewJournal';
+import JournalEntry from './containers/JournalEntry';
 
 class App extends React.Component {
   render() {
@@ -9,6 +12,11 @@ class App extends React.Component {
       <div>
         <Navbar />
         <UserPage />
+
+        {/* Routes */}
+        <Route path="/journals/new" component={NewJournal} />
+        <Route path="/journals/:id" component={JournalEntry} />
+
       </div>
     );
   }
