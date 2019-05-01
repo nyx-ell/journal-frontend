5c61a58ce3d413125e83f20af40baf618c60ef4f
nyx-ell <nicole.linyx@gmail.com>
2019-05-01 22:44:10 +0800

    updated page render and routing

--- src/App.js
+++ src/App.js
@@ -1,60 +1,24 @@
 import './App.css';
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Navbar from './components/navbar';
 import UserPage from './pages/UserPage';
 import NewJournal from './pages/NewJournal';
+import Navbar from './containers/navbar';
 import JournalEntry from './containers/JournalEntry';
+import Homepage from './pages/Homepage'
+
 import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      text: ''
-    }
-  }
-
-  onListen() {
-    fetch('http://localhost:3002/api/speech-to-text/token')
-      .then(function (response) {
-        return response.text();
-      }).then((token) => {
-        console.log('token is', token)
-        var stream = recognizeMic({
-          token: token, // use `access_token` as the parameter name if using an RC service
-          objectMode: true, // send objects instead of text
-          extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
-          format: false // optional - performs basic formatting on the results such as capitals an periods
-        });
-        stream.on('data', (data) => {
-          this.setState({
-            text: data.alternatives[0].transcript
-          })
-          console.log(data);
-        });
-        stream.on('error', function (err) {
-          console.log(err);
-        });
-        // document.querySelector('#stop').onclick = stream.stop.bind(stream);
-      }).catch(function (error) {
-        console.log(error);
-      });
-  }
-
   render() {
     return (
-      <div>
+      <>
         <Navbar />
-        <UserPage />
-
-        {/* Routes */}
+        <Route path="/" exact component={Homepage} />
+        <Route path="/journals" exact component={UserPage} />
         <Route path="/journals/new" component={NewJournal} />
         <Route path="/journals/:id" component={JournalEntry} />
-
-        <button onClick={this.onListen.bind(this)}>Record journal</button>
-        <div style={{ fontSize: '40px' }}>{this.state.text}</div>
-      </div>
+      </>
 
     );
   }
