bd614cecec9520438bc2270c8e69dc9b56f80da9
nyx-ell <nicole.linyx@gmail.com>
2019-05-03 00:24:34 +0800

    updated routes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Navbar from './containers/navbar';
 import JournalEntry from './containers/JournalEntry';
 import Homepage from './pages/Homepage'
 
-import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
-
 class App extends React.Component {
   render() {
     return (
@@ -17,7 +15,7 @@ class App extends React.Component {
         <Route path="/" exact component={Homepage} />
         <Route path="/journals" exact component={UserPage} />
         <Route path="/journals/new" component={NewJournal} />
-        <Route path="/journals/:id" component={JournalEntry} />
+        <Route path="/journals/:id(\d+)" component={JournalEntry} />
       </>
 
     );
