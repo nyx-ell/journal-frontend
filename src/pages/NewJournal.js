0960d29897ab51764730df354c002072a4caecff
nyx-ell <nicole.linyx@gmail.com>
2019-05-04 22:56:23 +0800

    updated aws upload and redirect

--- src/pages/NewJournal.js
+++ src/pages/NewJournal.js
@@ -11,22 +11,9 @@ class NewJournal extends React.Component {
             content: '',
             file: null,
             url: null,
-            submitStatus: false,
         };
         this.handleInput = this.handleInput.bind(this)
-        this.handleUpload = this.handleUpload.bind(this)
-    }
-
-    reset = () => {
-        if (this.state.submitStatus) {
-            this.setState({
-                title: '',
-                content: '',
-                file: null,
-                url: null,
-                submitStatus: false,
-            })
-        }
+        this.handleFile = this.handleFile.bind(this)
     }
 
     handleInput = (event) => {
@@ -35,43 +22,35 @@ class NewJournal extends React.Component {
         })
     }
 
-    handleUpload = (event) => {
+    handleFile = (event) => {
         this.setState({
             file: event.target.files[0],
             url: URL.createObjectURL(event.target.files[0]),
-        });
-        console.log(this.state.file)
+        })
     }
 
-    submitUpload = (event) => {
-        event.preventDefault();
-        const data = new FormData();
-        data.append('file', this.state.file);
-        console.log('user file is', this.state.file);
-    }
+    handleSubmit = (event) => {
+        const { title, content, file } = this.state
+        const fd = new FormData()
+        fd.set('user_id', localStorage.getItem('userId'))
+        fd.set('title', title)
+        fd.set('content', content)
+        fd.append('file', file, file.name)
 
-    handleSubmit = () => {
-        axios({
-            method: 'POST',
-            url: 'http://localhost:5000/api/v1/journals/new',
-            data: {
-                user_id: localStorage.getItem('userId'),
-                title: this.state.title,
-                content: this.state.content,
-                file: this.state.file
+        const token = localStorage.getItem('token')
+        axios.post('http://localhost:5000/api/v1/journals/new', fd, {
+            headers: {
+                Authorization: `Bearer ${token}`
             }
         })
             .then((response) => {
+                console.log(response)
                 if (response.data.status === "success") {
-                    this.setState({
-                        submitStatus: true,
-                    })
-
+                    if (response.data.redirect) {
+                        window.location.href = response.data.redirect;
+                    }
                     // On success, display success message
-                    console.log('Log in successful!')
-
-                    // Reset form
-                    this.reset()
+                    console.log('Journal created!')
                 } else {
                     // On response but email validation failure, display error message
                     console.log('Uh oh! Your journal entry could not be created')
@@ -86,24 +65,20 @@ class NewJournal extends React.Component {
         const { title, content, file } = this.state
         return (
             <>
-                <AvForm onValidSubmit={this.handleSubmit} id="journal">
+                <AvForm onValidSubmit={this.handleSubmit} id="journal" encType="multipart/form-data">
                     <AvField placeholder="Title" name="title" value={title} onChange={this.handleInput} id="title" type="text" validate={{
                         required: { value: true, errorMessage: 'Please give your journal a title' },
                         maxLength: { value: 255, errorMessage: 'Your title cannot exceed 255 characters' }
                     }} />
-
                     <textarea placeholder="Write away..." name="content" value={content} onChange={this.handleInput} id="content" type="text" className="form-control"></textarea><br />
-
-                    <form id="upload-form">
-                        <div className="input-group col-sm-6">
-                            <div className="custom-file">
-                                <input type="file" className="custom-file-input" id="file" onChange={this.handleUpload} placeholder="text2" />
-                                <label className="custom-file-label" for="file">{(file) ? file.name : "Choose an image"}</label>
-                            </div>
-                            <Button outline color="secondary" type="submit" onClick={this.submitUpload} id="upload-button">Upload</Button><br />
+                    <div className="input-group col-sm-6">
+                        <div className="custom-file">
+                            <input type="file" className="custom-file-input" id="file" onChange={this.handleFile} />
+                            <label className="custom-file-label" for="file">{(file) ? file.name : "Choose an image"}</label>
                         </div>
-                    </form> <br />
-                    <img src={this.state.url} className="img-thumbnail" />
+                    </div>
+                    <br />
+                    <img src={this.state.url} alt="" className="img-thumbnail" />
                     <br /><br />
                     <Button light color="secondary" form="journal" type="submit" className="float-right">Create new journal entry</Button>
                 </AvForm>
